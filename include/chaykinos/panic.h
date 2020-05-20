6258eed8fa5cff76891d83d22d79a57db08990f0
Simon Chaykin <52384223+game-lover@users.noreply.github.com>
2020-05-20 12:57:32 +0100

    Create panic.h

---
+++ include/chaykinos/panic.h
@@ -0,0 +1,16 @@
+#ifndef _CHAYKIN_OS_PANIC_H_
+#define _CHAYKIN_OS_PANIC_H_ 1
+
+#include <chaykinos/tty.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void panic(char*, ...);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
