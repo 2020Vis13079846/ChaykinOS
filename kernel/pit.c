b5aef71df207767ae4ee5450c564608488636cfe
Simon Chaykin <52384223+game-lover@users.noreply.github.com>
2020-05-21 14:49:42 +0100

    Create pit.c

---
+++ kernel/pit.c
@@ -0,0 +1,27 @@
+#include <chaykinos/interrupts.h>
+#include <asm/ports.h>
+
+#define PIT_FREQUENCY 100
+
+static uint64_t ticks = 0;
+
+static void pit_handler(__attribute__((unused)) registers_t* r) {
+	ticks++;
+}
+
+uint32_t pit_get_ticks(void) {
+	return ticks;
+}
+
+void pit_sleep(uint32_t time) {
+	uint64_t eticks = ticks + time;
+	while (pit_get_ticks() < eticks);
+}
+
+void pit_init(void) {
+	register_interrupt_handler(32, &pit_handler);
+	int divisor = 1193180 / PIT_FREQUENCY;
+	outb(0x43, 0x36);
+	outb(0x40, divisor & 0xff);
+	outb(0x40, divisor >> 8);
+}
