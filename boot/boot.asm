f930ec8c6525c01068d3f0825b5488cd499cd92b
Simon Chaykin <52384223+game-lover@users.noreply.github.com>
2020-06-06 12:44:53 +0100

    Update boot.asm

--- boot/boot.asm
+++ boot/boot.asm
@@ -16,10 +16,39 @@ align 4 ; 4-byte aligned code.
 ; Start of the bootstrap stack for kernel.
 section .bss
 stack_bottom:
-resb 16384 ; 16 KiB for the stack.
+resb 65536 ; 64 KiB for the stack.
 stack_top: ; Start of the stack.
 
+KERNEL_VIRTUAL_BASE equ 0xC0000000                  ; 3GB
+KERNEL_PAGE_NUMBER  equ (KERNEL_VIRTUAL_BASE >> 22) ; Kernel page direcory
+
+section .data
+align 0x1000
+global _boot_page_directory
+_boot_page_directory:
+	; This page directory entry identity-maps the first 4MB of the 32-bit physical address space.	dd 0x00000083
+	times (KERNEL_PAGE_NUMBER - 1) dd 0
+	; This page directory entry defines a 4MB page containing the kernel.
+	dd 0x00000083
+	times (1024 - KERNEL_PAGE_NUMBER - 1) dd 0
+
 section .text
+global _loader
+_loader:
+	mov ecx, (_boot_page_directory - KERNEL_VIRTUAL_BASE)
+	mov cr3, ecx ; Load Page directory base register.
+
+	mov ecx, cr4
+	or ecx, 0x00000010 ; Set PSE bit in CR4 to enable 4MB pages.
+	mov cr4, ecx
+
+	mov ecx, cr0
+	or ecx, 0x80000000 ; Set PG bit in CR0 to enable paging.
+	mov cr0, ecx
+
+	lea ecx, [_start]
+	jmp ecx
+
 global _start
 _start:
 	; Disable interrupts.
@@ -37,9 +66,16 @@ _start:
 	push ebx ; Multiboot structure.
 	push eax ; Magic number.
 
-	; Running kernel.
+	; Calling early kernel initialization.
 	extern start_kernel
 	call start_kernel
+
+	; Enter protected mode.
+	mov ecx, cr0
+	or cl, 1
+	mov cr0, ecx
+
+	; Running kernel
 
 	extern main
 	call main
