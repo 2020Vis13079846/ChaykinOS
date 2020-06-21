0ba3196bfeefb13ece5bfbae79d264ed1ad859b2
Simon Chaykin <52384223+game-lover@users.noreply.github.com>
2020-06-21 17:47:16 +0100

    Create keyboard.h

---
+++ include/chaykinos/keyboard.h
@@ -0,0 +1,31 @@
+#ifndef _CHAYKIN_OS_KEYBOARD_H_
+#define _CHAYKIN_OS_KEYBOARD_H_ 1
+
+#include <stdint.h>
+#include <stddef.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct {
+	uint8_t scancode;
+	char keycode;
+} char_t;
+
+typedef struct {
+	uint8_t exists;
+	uint8_t release;
+	char keycode;
+} kbd_char_t;
+
+void keyboard_init(void);
+
+uint8_t keyboard_getchar(void);
+size_t keyboard_gets(char*, size_t);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
