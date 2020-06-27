2da21a22ef4c21408193b331aa92d6d5a6efda75
Simon Chaykin <52384223+game-lover@users.noreply.github.com>
2020-06-27 14:35:26 +0100

    Update string.h

--- include/string.h
+++ include/string.h
@@ -9,11 +9,11 @@
 extern "C" {
 #endif
 
-int memcmp(const void*, const void*, size_t);
-void* memcpy(void*, const void*, size_t);
-void* memmove(void*, const void*, size_t);
-void* memset(void*, int, size_t);
-size_t strlen(char*);
+int memcmp(const void *, const void *, size_t);
+void* memcpy(void *, const void *, size_t);
+void* memmove(void *, const void *, size_t);
+void* memset(void *, int, size_t);
+size_t strlen(char *);
 
 #ifdef __cplusplus
 }
