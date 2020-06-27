c81c507fac273f1265af7b7829e690da3b7993fb
Simon Chaykin <52384223+game-lover@users.noreply.github.com>
2020-06-27 14:30:56 +0100

    Update pit.h

--- include/chaykinos/pit.h
+++ include/chaykinos/pit.h
@@ -1,8 +1,7 @@
 #ifndef _CHAYKIN_OS_PIT_H_
 #define _CHAYKIN_OS_PIT_H_ 1
 
-#include <chaykinos/interrupts.h>
-#include <asm/ports.h>
+#include <stdint.h>
 
 #define PIT_FREQUENCY 100
 
@@ -10,9 +9,10 @@
 extern "C" {
 #endif
 
-uint32_t pit_get_ticks(void);
-void pit_sleep(uint32_t time);
 void pit_init(void);
+
+uint32_t pit_get_ticks(void);
+void pit_sleep(uint32_t);
 
 #ifdef __cplusplus
 }
